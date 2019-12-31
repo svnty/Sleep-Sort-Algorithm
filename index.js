fde722ff9b25625712a19da85c2f9d7b58b45fdf
Jake Walklate <jakewalklate@gmail.com>
2019-12-31 15:11:23 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const sleepSort = async (arr) => {
+  const sorted = [];
+  const neg = [];
+  const pos = [];
+  arr.forEach((element) => {
+    if (element < 0) {
+      neg.push(element *= -1);
+    } else {
+      pos.push(element);
+    }
+  });
+  await Promise.all(
+    pos.map((n) => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(sorted.push(n));
+      }, n <= 10 ? n * 10 : n);
+    }))
+  );
+  await Promise.all(
+    neg.map((n) => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(sorted.unshift(n *= -1));
+      }, n <= 10 ? n * 10 : n);
+    }))
+  );
+  return sorted;
+};
+
+sleepSort([4, -1, -2, 5, 1, 4, 5, 9, 3, 2, 1, -9]).then(console.log);
